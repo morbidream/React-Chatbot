9b3d8b9c04fd0c89a69528dbe63d345730b22679
morbi <elae.debichi@esprit.tn>
2022-05-07 23:15:34 +0100

    deployed 🤞

--- src/config.js
+++ src/config.js
@@ -1,7 +1,24 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 
 const config = {
-  initialMessages: [createChatBotMessage(`Welcome to my chatbot! say "hello" 🤭`)]
+  initialMessages: [createChatBotMessage(`Hi my name is Tessa!`), 
+  createChatBotMessage("say 'hello' 🤭", {
+    withAvatar: true,
+    delay: 1000,
+    widget: "overview",
+  })],
+  botName: "Tessa",
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: '#376B7E',
+    },
+    chatButton: {
+      backgroundColor: '#5ccc9d',
+    },
+  },
+  state: {
+    myCustomProperty: 'Bikershorts',
+  },
 };
 
 export default config;
