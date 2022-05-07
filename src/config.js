507a5c59483f8adcd027f02198f6eba342745b48
morbi <elae.debichi@esprit.tn>
2022-05-07 22:41:03 +0100

    deployed 🤞

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 
 const config = {
-  initialMessages: [createChatBotMessage(`Welcome to my chatbot!`)],
+  initialMessages: [createChatBotMessage(`Welcome to my chatbot! say "hello" 🤭`)]
 };
 
 export default config;
